80c9b6d4b2d08d86467ee0026c226d5801319424
PJC-1 <jumpzine@gmail.com>
2018-10-07 20:12:36 -0700

    added Movement.cs script

---
+++ Movement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float forceApplied = 20f;
+    public Rigidbody rb;
+
+    void FixedUpdate() {
+
+    }
+}
