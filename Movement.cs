612e7583f73b7df6cdaf25bb5c460d78d727d2ce
PJC-1 <jumpzine@gmail.com>
2018-10-08 21:09:47 -0700

    added key input and applied force to the rigidbody

--- Movement.cs
+++ Movement.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
+    // amount of force applied to rigidbody
     public float forceApplied = 20f;
+
+    // holds rigidbody
     public Rigidbody rb;
 
+    // function that updates every physics updates
+    // "Fixed" will update every physics update, rather than every frame
+    // "void" is used when the function does not return anything
     void FixedUpdate() {
-
+      //Get input from the user
+      if(Input.GetKey("w")){
+        //Apply force to rigidbody
+        rb.AddForce(0,0, forceApplied * Time.deltaTime);
+      }
     }
 }
