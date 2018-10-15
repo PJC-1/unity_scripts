e74d743a815f973438ff863d7a1850231321b273
PJC-1 <jumpzine@gmail.com>
2018-10-14 22:19:25 -0700

    adding a move script for cube it game

---
+++ orange/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    // grab the rigidbody
+    public Rigidbody rb;
+
+    // create a variable that will hold the amount of force being applied
+    public float forceApplied = 40f;
+
+    public float forwardForce = 1000f;
+
+    // check to see what key was pressed
+    void FixedUpdate() {
+        // what key did the user press?
+        // move forward
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(forceApplied * Time.deltaTime, 0, 0);
+        }
+
+        if(Input.GetKey("a")){
+            rb.AddForce( -forceApplied * Time.deltaTime, 0, 0);
+        }
+    }
+}
