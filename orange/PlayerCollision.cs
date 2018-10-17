f2bba1bff5a4ce68003897944504f4b550f98b37
PJC-1 <jumpzine@gmail.com>
2018-10-16 21:37:00 -0700

    implemented PlayerMovement.cs script

---
+++ orange/PlayerCollision.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    // get a reference to movement script
+    public PlayerMovement movement;
+
+    // fires whenever collision happens
+    void OnCollisionEnter(Collision collisionInfo) {
+        // fire some script only when the player hits an obstacle
+        if(collisionInfo.collider.tag == "Obstacle"){
+            // stop player movement
+            movement.enabled = false;
+        }
+    }
+}
