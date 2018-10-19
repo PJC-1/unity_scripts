67acda4b8ee9b1e573fc6a600c4094432f1be0c7
PJC-1 <jumpzine@gmail.com>
2018-10-18 21:31:04 -0700

    added UI score script

---
+++ orange/Score.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    // get the z position of the player
+    public Transform player;
+    // get the UI text
+    public Text scoreText;
+
+
+	// Update is called once per frame
+	void Update () {
+		// update the score text with the z position of the player
+        scoreText.text = player.position.z.ToString("0");
+	}
+}
