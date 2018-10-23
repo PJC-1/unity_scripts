959684f451e041196e30c58678b7fc30d0dfbc2b
PJC-1 <jumpzine@gmail.com>
2018-10-22 22:20:16 -0700

    implemented a start script

---
+++ orange/StartMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public void OnTriggerEnter(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
